b1f980a933f6c085252173f5c345937b51aab269
Kristjan <kristjan@causes.com>
2008-12-20 23:05:24 -0800

    Enable poing checking

--- main.py
+++ main.py
@@ -7,35 +7,45 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
 class Poing(db.Model):
-	poinger = db.StringProperty(multiline=False)
-	poingee = db.StringProperty(multiline=False)
+	poinger = db.StringProperty()
+	poingee = db.StringProperty()
 	poinged = db.DateTimeProperty(auto_now=True)
 
+class SmartHandler(webapp.RequestHandler):
+	def render(self, filename, data=None):
+		path = os.path.join(os.path.dirname(__file__), filename+'.html')
+		self.response.out.write(template.render(path, data or {}))
 
-class MainHandler(webapp.RequestHandler):
-	
+class MainHandler(SmartHandler):
 	def get(self):
 		self.render('index')
 
+class PoingHandler(SmartHandler):
+	def get(self):
+		poinger = self.request.get('poinger')
+		poingee = self.request.get('poingee')
+
+		poing = Poing.get_by_key_name("%s_%s" % (poinger, poingee))
+
+		data = { 'poing': poing }
+		self.render('poinged', data)
+
 	def post(self):
-		poinger = self.request.get('from')
-		poingee = self.request.get('to')
+		poinger = self.request.get('poinger')
+		poingee = self.request.get('poingee')
 
 		poing = Poing.get_or_insert("%s_%s" % (poinger, poingee),
 																poinger=poinger, poingee=poingee)
 		poing.put()
+		poing = Poing.get(poing.key()) # Reload timestamp from put()
 
 		data = { 'poing': poing }
 		self.render('poinged', data)
 
 
-	def render(self, filename, data=None):
-		path = os.path.join(os.path.dirname(__file__), filename+'.html')
-		self.response.out.write(template.render(path, data or {}))
-
-
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+  application = webapp.WSGIApplication([('/', MainHandler),
+																				('/poing', PoingHandler)], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
 
